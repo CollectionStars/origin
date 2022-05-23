fb273d2fd9d477618279d07e8a2366f8ca11677e
CollectionStars <104120690+CollectionStars@users.noreply.github.com>
2022-05-23 21:41:49 +0300

    update Source.cpp

--- IDE/Source.cpp
+++ IDE/Source.cpp
@@ -22,7 +22,7 @@ int main() {
 	int size1;
 	fin >> size1;
 	
-	if (size1 < 0) 
+	if (size1 <= 0) 
 	{
 		std::cout << "Некорректный размер массива!\n";
 		return 0;
@@ -38,7 +38,7 @@ int main() {
 	int size2;
 	fin >> size2;
 
-	if (size2 < 0) 
+	if (size2 <= 0) 
 	{
 		std::cout << "Некорректный размер массива!\n";
 		return 0;
