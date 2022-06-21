5228f3c261a775af0cd478060ce7875ef4188f8e
CollectionStars <104120690+CollectionStars@users.noreply.github.com>
2022-06-21 23:14:25 +0300

    repair Rhombus.h

--- Исключения и обработка ошибок/Задача 2. Исключения в конструкторах/Rhombus.h
+++ Исключения и обработка ошибок/Задача 2. Исключения в конструкторах/Rhombus.h
@@ -6,7 +6,7 @@
 //a==b==c==d, A==C B==D
 class Rhombus : public Parallelogram {
 public:
-	Rhombus(int a_, int A_, int B_, std::string n = "Ромб");
+	Rhombus(int a_, int A_, int B_, std::string n = "Р РѕРјР±");
 };
 
 #endif // !_RHOMBUS_H_
