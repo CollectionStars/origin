ca2bc57b077b31a1a8bb2c1a3e518be7c8ac7fcc
CollectionStars <104120690+CollectionStars@users.noreply.github.com>
2022-06-26 13:45:40 +0300

    repair Rect_Triangle.h

--- Исключения и обработка ошибок/Задача 2. Исключения в конструкторах/Rect_Triangle.h
+++ Исключения и обработка ошибок/Задача 2. Исключения в конструкторах/Rect_Triangle.h
@@ -1,7 +1,7 @@
 #pragma once 
 
 #include "Triangle.h"
-//прямоугольный треугольник
+//РїСЂСЏРјРѕСѓРіРѕР»СЊРЅС‹Р№ С‚СЂРµСѓРіРѕР»СЊРЅРёРє
 class Rect_Triangle :public Triangle {
 public:
 	Rect_Triangle(int a_, int b_, int c_, int A_, int B_);
