88c4d7e63ddbbf2407b08fd640a41096d93ace67
CollectionStars <104120690+CollectionStars@users.noreply.github.com>
2022-06-26 13:39:53 +0300

    repair

--- Исключения и обработка ошибок/Задача 2. Исключения в конструкторах/Equ_Triangle.cpp
+++ Исключения и обработка ошибок/Задача 2. Исключения в конструкторах/Equ_Triangle.cpp
@@ -1,5 +1,5 @@
 #include "Equ_Triangle.h"
 
 Equ_Triangle::Equ_Triangle(int a_) :Iso_Triangle(a_, a_, 60, 60) {
-	name = "Равносторонний треугольник";
+	name = "Р Р°РІРЅРѕСЃС‚РѕСЂРѕРЅРЅРёР№ С‚СЂРµСѓРіРѕР»СЊРЅРёРє";
 }
