b389b3354ddf6e1e485e35ab8640443dad43628b
CollectionStars <104120690+CollectionStars@users.noreply.github.com>
2022-06-26 13:45:13 +0300

    repair Rect_Triangle.cpp

--- Исключения и обработка ошибок/Задача 2. Исключения в конструкторах/Rect_Triangle.cpp
+++ Исключения и обработка ошибок/Задача 2. Исключения в конструкторах/Rect_Triangle.cpp
@@ -3,5 +3,5 @@
 
 Rect_Triangle::Rect_Triangle(int a_, int b_, int c_, int A_, int B_) :Triangle(a_, b_, c_, A_, B_, 90)
 {
-	name = "Прямоугольный треугольник";
+	name = "РџСЂСЏРјРѕСѓРіРѕР»СЊРЅС‹Р№ С‚СЂРµСѓРіРѕР»СЊРЅРёРє";
 }
